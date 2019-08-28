3083fd1a8fcdf0e835392f6ff4e902bdb54c726b
eiccu <eijaleena.koponen@gmail.com>
2019-08-28 15:01:25 +0300

    Better example

--- predict/app.R
+++ predict/app.R
@@ -26,19 +26,19 @@ ui <- tagList(
         "Deploy demo", 
         
         ################### TAB amazon   ################### 
-        tabPanel(title = "Amazon reviews (EN)",
+        tabPanel(title = "Toxic content (EN)",
                  #setBackgroundImage(src="logo-white@2000x1910.png"),
                  h2("Review predictor demo"),
                  # input field
                  p("Give a review:"),
-                 textAreaInput("user_text_en", label = NULL, 
+                 textAreaInput("user_text_toxic", label = NULL, 
                                placeholder = "Please enter some text.",width = "800px"
                  ),
                  
                  # display text output
                  hr(),
                  h4("Inputted description"),
-                 textOutput("text_en"),
+                 textOutput("text_toxic"),
                  tags$head(tags$style("#text1{font-style: italic;
                                   }"
                  )
@@ -47,12 +47,39 @@ ui <- tagList(
                  # change style:    
                  #,tags$head(tags$style("#view table {background-color: white; }", media="screen", type="text/css"))
                  hr(),
-                 h4("Model output for reviews and their similarities"),
+                 h4("Model output for toxicity and their similarities"),
                  
-                 tableOutput("view_table_en") %>% withSpinner(color="#0dc5c1")
+                 tableOutput("view_table_toxic") %>% withSpinner(color="#0dc5c1")
                  
         )#end of tabPanel
         ,
+        # tabPanel(title = "Amazon reviews (EN)",
+        #          #setBackgroundImage(src="logo-white@2000x1910.png"),
+        #          h2("Review predictor demo"),
+        #          # input field
+        #          p("Give a review:"),
+        #          textAreaInput("user_text_en", label = NULL, 
+        #                        placeholder = "Please enter some text.",width = "800px"
+        #          ),
+        #          
+        #          # display text output
+        #          hr(),
+        #          h4("Inputted description"),
+        #          textOutput("text_en"),
+        #          tags$head(tags$style("#text1{font-style: italic;
+        #                           }"
+        #          )
+        #          )
+        #          ,
+        #          # change style:    
+        #          #,tags$head(tags$style("#view table {background-color: white; }", media="screen", type="text/css"))
+        #          hr(),
+        #          h4("Model output for reviews and their similarities"),
+        #          
+        #          tableOutput("view_table_en") %>% withSpinner(color="#0dc5c1")
+        #          
+        # )#end of tabPanel
+        # ,
         
         tabPanel(title = "Ruimtehol example (NL)",
                  #setBackgroundImage(src="logo-white@2000x1910.png"),
@@ -88,23 +115,35 @@ ui <- tagList(
 server <- function(input, output) {
     
     # text output for the first page
-    output$text_en <- renderText({
-        input$user_text_en
+    output$text_toxic <- renderText({
+        input$user_text_toxic
     })
     
     
-    ##amazon reviews
+    ##toxicity
     
-    output$view_table_en <- renderTable({
-        make_prediction_en(input$user_text_en) %>%
+    output$view_table_toxic <- renderTable({
+        make_prediction_en(input$user_text_toxic) %>%
             top_n(5)
     },hover=TRUE)
-    
-    
     # text output for the first page
-    output$text_nl <- renderText({
-        input$user_text_nl
+    output$text_toxic <- renderText({
+        input$user_text_toxic
     })
+    
+    
+    # ##amazon reviews
+    # 
+    # output$view_table_en <- renderTable({
+    #     make_prediction_en(input$user_text_en) %>%
+    #         top_n(5)
+    # },hover=TRUE)
+    # 
+    # 
+    # # text output for the first page
+    # output$text_nl <- renderText({
+    #     input$user_text_nl
+    # })
     
     
     ##ruimtehol example
